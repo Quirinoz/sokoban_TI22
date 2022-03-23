c151dfe7cd25fa8cb474bf3c86e871132b3cddde
Miguel Angel Rodriguez Quirino <97248509+Quirinoz@users.noreply.github.com>
2022-03-23 20:45:05 +0000

    Imprimir el mapa y  empieza a jugar

--- sokoban.py
+++ sokoban.py
@@ -1,1 +1,26 @@
-print("hola")
+class Sokoban:
+  mapa = [
+    [3,3,3,3,3,3,3],
+    [3,1,1,1,1,1,3],
+    [3,1,1,1,1,1,3],
+    [3,1,1,0,1,1,3],
+    [3,1,1,1,1,1,3],
+    [3,1,1,1,1,1,3],
+    [3,3,3,3,3,3,3]
+  ]
+  #mapa donde uno puede representarse y poder moverse
+  personaje_fila = 3 #fila en la que se encuentra el personaje
+  personaje_columna = 3 #columna en la que se encuentra el personaje 
+#variables que ubican la posición del personaje en el mapa
+  def imprimirMapa(self):
+    for fila in self.mapa:
+      print(fila)
+      
+  def jugar(self):
+    while True:
+      self.imprimirMapa()
+      movimiento = input("Moverse")
+
+juego = Sokoban()
+juego.jugar()
+
