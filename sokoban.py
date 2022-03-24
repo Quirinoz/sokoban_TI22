838cf0e76693d4f648cc21f3bca045bf4274b7fa
Miguel Angel Rodriguez Quirino <97248509+Quirinoz@users.noreply.github.com>
2022-03-24 02:11:40 +0000

    paso 3 de 12 listo

--- sokoban.py
+++ sokoban.py
@@ -1,12 +1,12 @@
 class Sokoban:
   mapa = [
-    [3,3,3,3,3,3,3],
-    [3,1,1,1,1,1,3],
-    [3,1,1,1,1,1,3],
-    [3,1,1,0,4,1,3],
-    [3,1,1,1,1,1,3],
-    [3,1,1,1,1,1,3],
-    [3,3,3,3,3,3,3]
+    [3,3,3,3,3,3,3,3,3,3,3,3,3,3,3],
+    [3,1,1,1,1,1,1,1,1,1,1,1,1,1,3],
+    [3,1,1,1,1,1,1,1,1,1,1,1,1,1,3],
+    [3,1,1,0,4,4,1,1,1,1,1,1,1,1,3],
+    [3,1,1,1,1,1,1,1,1,1,1,1,1,1,3],
+    [3,1,1,1,1,1,1,1,1,1,1,1,1,1,3],
+    [3,3,3,3,3,3,3,3,3,3,3,3,3,3,3]
   ]
   #mapa donde uno puede representarse y poder moverse
   personaje_fila = 3 #fila en la que se encuentra el personaje
@@ -21,10 +21,19 @@ class Sokoban:
       self.mapa[self.personaje_fila][self.personaje_columna] = 1
       self.mapa[self.personaje_fila][self.personaje_columna + 1] = 0
       self.personaje_columna += 1 #solo es la unidad actualizada del movimiento
+      
     elif self.mapa[self.personaje_fila][self.personaje_columna] == 0 and self.mapa[self.personaje_fila][self.personaje_columna + 1] == 4:
       self.mapa[self.personaje_fila][self.personaje_columna] = 1
       self.mapa[self.personaje_fila][self.personaje_columna + 1] = 5
       self.personaje_columna += 1 #solo es la unidad actualizada del movimiento
+
+    elif self.mapa[self.personaje_fila][self.personaje_columna] == 5 and self.mapa[self.personaje_fila][self.personaje_columna + 1] == 4:
+      self.mapa[self.personaje_fila][self.personaje_columna] = 4
+      self.mapa[self.personaje_fila][self.personaje_columna + 1] = 5
+      self.personaje_columna += 1 #solo es la unidad actualizada del movimiento
+
+    elif self.mapa[self.personaje_fila][self.personaje_columna] == 5 and self.mapa[self.personaje_fila][self.personaje_columna + 1] == 1:
+      self.mapa[self.personaje_fila][self.personaje_columna] = 4
   def jugar(self):# Controla el flujo del juego
     while True:# Si es verdadera
       self.imprimirMapa()
