0196310388b0be0fb05089cc57b639ffdb388df9
Miguel Angel Rodriguez Quirino <97248509+Quirinoz@users.noreply.github.com>
2022-03-25 17:44:38 +0000

    personaje_meta, caja_meta, espacio #11

--- sokoban.py
+++ sokoban.py
@@ -90,6 +90,12 @@ class Sokoban:
       self.mapa[self.personaje_fila][self.personaje_columna + 1] = 2
       self.personaje_columna += 1 #solo es la unidad actualizada del movimiento
 
+  # 11 (personaje_meta, caja_meta_espacio)
+    elif self.mapa[self.personaje_fila][self.personaje_columna] == 5 and self.mapa[self.personaje_fila][self.personaje_columna + 1] == 6 and self.mapa[self.personaje_fila][self.personaje_columna + 1] == 4:
+      self.mapa[self.personaje_fila][self.personaje_columna] = 4
+      self.mapa[self.personaje_fila][self.personaje_columna + 1] = 5
+      self.mapa[self.personaje_fila][self.personaje_columna + 1] = 6
+      self.personaje_columna += 1 #solo es la unidad actualizada del movimiento
 
   def jugar(self):# Controla el flujo del juego
     while True:# Si es verdadera
