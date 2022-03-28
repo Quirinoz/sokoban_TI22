6b04f0aaa5995b9445867b1bc37a4fe6bbbdc18d
Miguel Angel Rodriguez Quirino <97248509+Quirinoz@users.noreply.github.com>
2022-03-28 20:16:24 +0000

    El personaje ya puede mover la caja y este avanza con el

--- sokoban.py
+++ sokoban.py
@@ -3,7 +3,7 @@ class Sokoban:
     [3,3,3,3,3,3,3,3,3,3,3,3,3,3,3],
     [3,1,1,1,1,1,1,1,1,1,1,1,1,1,3],
     [3,1,1,1,1,1,1,1,1,1,1,1,1,1,3],
-    [3,1,1,0,1,1,1,1,1,1,1,1,1,1,3],
+    [3,1,1,0,1,2,1,1,1,1,1,1,1,1,3],
     [3,1,1,1,1,1,1,1,1,1,1,1,1,1,3],
     [3,1,1,1,1,1,1,1,1,1,1,1,1,1,3],
     [3,3,3,3,3,3,3,3,3,3,3,3,3,3,3]
@@ -44,31 +44,31 @@ class Sokoban:
       self.personaje_columna += 1 #solo es la unidad actualizada del movimiento
 
   # 2 (personaje, caja, espacio)
-    elif self.mapa[self.personaje_fila][self.personaje_columna] == 0 and self.mapa[self.personaje_fila][self.personaje_columna + 1] == 2 and self.mapa[self.personaje_fila][self.personaje_columna + 1] == 1:
+    elif self.mapa[self.personaje_fila][self.personaje_columna] == 0 and self.mapa[self.personaje_fila][self.personaje_columna + 1] == 2 and self.mapa[self.personaje_fila][self.personaje_columna + 2] == 1:
       self.mapa[self.personaje_fila][self.personaje_columna] = 1
       self.mapa[self.personaje_fila][self.personaje_columna + 1] = 0
-      self.mapa[self.personaje_fila][self.personaje_columna + 1] = 2
+      self.mapa[self.personaje_fila][self.personaje_columna + 2] = 2
       self.personaje_columna += 1 #solo es la unidad actualizada del movimiento
 
   # 3 (personaje, caja, meta)
-    elif self.mapa[self.personaje_fila][self.personaje_columna] == 0 and self.mapa[self.personaje_fila][self.personaje_columna + 1] == 2 and self.mapa[self.personaje_fila][self.personaje_columna + 1] == 4:
+    elif self.mapa[self.personaje_fila][self.personaje_columna] == 0 and self.mapa[self.personaje_fila][self.personaje_columna + 1] == 2 and self.mapa[self.personaje_fila][self.personaje_columna + 2] == 4:
       self.mapa[self.personaje_fila][self.personaje_columna] = 1
       self.mapa[self.personaje_fila][self.personaje_columna + 1] = 0
-      self.mapa[self.personaje_fila][self.personaje_columna + 1] = 6
+      self.mapa[self.personaje_fila][self.personaje_columna + 2] = 6
       self.personaje_columna += 1 #solo es la unidad actualizada del movimiento
 
   # 4 (personaje, caja_meta, espacio)
-    elif self.mapa[self.personaje_fila][self.personaje_columna] == 0 and self.mapa[self.personaje_fila][self.personaje_columna + 1] == 6 and self.mapa[self.personaje_fila][self.personaje_columna + 1] == 1:
+    elif self.mapa[self.personaje_fila][self.personaje_columna] == 0 and self.mapa[self.personaje_fila][self.personaje_columna + 1] == 6 and self.mapa[self.personaje_fila][self.personaje_columna + 2] == 1:
       self.mapa[self.personaje_fila][self.personaje_columna] = 1
       self.mapa[self.personaje_fila][self.personaje_columna + 1] = 5
-      self.mapa[self.personaje_fila][self.personaje_columna + 1] = 2
+      self.mapa[self.personaje_fila][self.personaje_columna + 2] = 2
       self.personaje_columna += 1 #solo es la unidad actualizada del movimiento
 
   # 5 (personaje, caja_meta, meta)
-    elif self.mapa[self.personaje_fila][self.personaje_columna] == 0 and self.mapa[self.personaje_fila][self.personaje_columna + 1] == 6 and self.mapa[self.personaje_fila][self.personaje_columna + 1] == 4:
+    elif self.mapa[self.personaje_fila][self.personaje_columna] == 0 and self.mapa[self.personaje_fila][self.personaje_columna + 1] == 6 and self.mapa[self.personaje_fila][self.personaje_columna + 2] == 4:
       self.mapa[self.personaje_fila][self.personaje_columna] = 1
       self.mapa[self.personaje_fila][self.personaje_columna + 1] = 5
-      self.mapa[self.personaje_fila][self.personaje_columna + 1] = 6
+      self.mapa[self.personaje_fila][self.personaje_columna + 2] = 6
       self.personaje_columna += 1 #solo es la unidad actualizada del movimiento
 
   # 6 (personaje_meta, espacio)
@@ -84,31 +84,31 @@ class Sokoban:
       self.personaje_columna += 1 #solo es la unidad actualizada del movimiento
 
   # 8 (personaje, caja_meta, espacio)
-    elif self.mapa[self.personaje_fila][self.personaje_columna] == 5 and self.mapa[self.personaje_fila][self.personaje_columna + 1] == 2 and self.mapa[self.personaje_fila][self.personaje_columna + 1] == 1:
+    elif self.mapa[self.personaje_fila][self.personaje_columna] == 5 and self.mapa[self.personaje_fila][self.personaje_columna + 1] == 2 and self.mapa[self.personaje_fila][self.personaje_columna + 2] == 1:
       self.mapa[self.personaje_fila][self.personaje_columna] = 4
       self.mapa[self.personaje_fila][self.personaje_columna + 1] = 0
-      self.mapa[self.personaje_fila][self.personaje_columna + 1] = 2
+      self.mapa[self.personaje_fila][self.personaje_columna + 2] = 2
       self.personaje_columna += 1 #solo es la unidad actualizada del movimiento
 
   # 9 (personaje_meta, caja, meta)
-    elif self.mapa[self.personaje_fila][self.personaje_columna] == 5 and self.mapa[self.personaje_fila][self.personaje_columna + 1] == 2 and self.mapa[self.personaje_fila][self.personaje_columna + 1] == 4:
+    elif self.mapa[self.personaje_fila][self.personaje_columna] == 5 and self.mapa[self.personaje_fila][self.personaje_columna + 1] == 2 and self.mapa[self.personaje_fila][self.personaje_columna + 2] == 4:
       self.mapa[self.personaje_fila][self.personaje_columna] = 4
       self.mapa[self.personaje_fila][self.personaje_columna + 1] = 0
-      self.mapa[self.personaje_fila][self.personaje_columna + 1] = 6
+      self.mapa[self.personaje_fila][self.personaje_columna + 2] = 6
       self.personaje_columna += 1 #solo es la unidad actualizada del movimiento
 
   # 10 (personaje_meta, caja_meta, espacio)
-    elif self.mapa[self.personaje_fila][self.personaje_columna] == 5 and self.mapa[self.personaje_fila][self.personaje_columna + 1] == 6 and self.mapa[self.personaje_fila][self.personaje_columna + 1] == 1:
+    elif self.mapa[self.personaje_fila][self.personaje_columna] == 5 and self.mapa[self.personaje_fila][self.personaje_columna + 1] == 6 and self.mapa[self.personaje_fila][self.personaje_columna + 2] == 1:
       self.mapa[self.personaje_fila][self.personaje_columna] = 4
       self.mapa[self.personaje_fila][self.personaje_columna + 1] = 5
-      self.mapa[self.personaje_fila][self.personaje_columna + 1] = 2
+      self.mapa[self.personaje_fila][self.personaje_columna + 2] = 2
       self.personaje_columna += 1 #solo es la unidad actualizada del movimiento
 
   # 11 (personaje_meta, caja_meta_, meta)
-    elif self.mapa[self.personaje_fila][self.personaje_columna] == 5 and self.mapa[self.personaje_fila][self.personaje_columna + 1] == 6 and self.mapa[self.personaje_fila][self.personaje_columna + 1] == 4:
+    elif self.mapa[self.personaje_fila][self.personaje_columna] == 5 and self.mapa[self.personaje_fila][self.personaje_columna + 1] == 6 and self.mapa[self.personaje_fila][self.personaje_columna + 2] == 4:
       self.mapa[self.personaje_fila][self.personaje_columna] = 4
       self.mapa[self.personaje_fila][self.personaje_columna + 1] = 5
-      self.mapa[self.personaje_fila][self.personaje_columna + 1] = 6
+      self.mapa[self.personaje_fila][self.personaje_columna + 2] = 6
       self.personaje_columna += 1 #solo es la unidad actualizada del movimiento
 
   def jugar(self):# Controla el flujo del juego
