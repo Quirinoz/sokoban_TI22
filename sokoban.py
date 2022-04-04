e338b869c9998ea1d154d81ed7fac3e74ab310f0
Miguel Angel Rodriguez Quirino <97248509+Quirinoz@users.noreply.github.com>
2022-04-04 19:27:19 +0000

    #7 (personaje_meta, meta) arriba

--- sokoban.py
+++ sokoban.py
@@ -246,6 +246,12 @@ class Sokoban:
       self.mapa[self.personaje_fila][self.personaje_columna] = 4
       self.mapa[self.personaje_fila - 1][self.personaje_columna] = 0
       self.personaje_fila -= 1
+
+  # 7 (personaje_meta, meta)
+    elif self.mapa[self.personaje_fila][self.personaje_columna] == 5 and self.mapa[self.personaje_fila - 1][self.personaje_columna] == 4:
+      self.mapa[self.personaje_fila][self.personaje_columna] = 4
+      self.mapa[self.personaje_fila - 1][self.personaje_columna] = 5
+      self.personaje_fila -=1
       
   def moverAbajo(self):
     print("mover abajo")
