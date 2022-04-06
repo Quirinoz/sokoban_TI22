88a35ff71ae194108b9a15c1cd4f4e1e439d81f0
Miguel Angel Rodriguez Quirino <97248509+Quirinoz@users.noreply.github.com>
2022-04-06 15:53:37 +0000

    #9 (personaje_meta, caja, meta) abajo

--- sokoban.py
+++ sokoban.py
@@ -323,7 +323,7 @@ class Sokoban:
       self.mapa[self.personaje_fila][self.personaje_columna] = 1
       self.mapa[self.personaje_fila + 1][self.personaje_columna] = 5
       self.mapa[self.personaje_fila + 2][self.personaje_columna] = 6
-      self.persoanje_fila += 1
+      self.personaje_fila += 1
 
   # 6 (personaje_meta, espacio)
     elif self.mapa[self.personaje_fila][self.personaje_columna] == 5 and self.mapa[self.personaje_fila + 1][self.personaje_columna] == 1:
@@ -344,6 +344,13 @@ class Sokoban:
       self.persoanje_fila += 1
 
   # 9 (personaje_meta, caja, meta)
+    elif self.mapa[self.personaje_fila][self.personaje_columna] == 5 and self.mapa[self.personaje_fila + 1][self.personaje_columna] == 2 and self.mapa[self.personaje_fila + 2][self.personaje_columna] == 4:
+      self.mapa[self.personaje_fila][self.personaje_columna] = 4
+      self.mapa[self.personaje_fila + 1][self.personaje_columna] = 0
+      self.mapa[self.personaje_fila + 2][self.personaje_columna] = 6
+      self.personaje_fila += 1
+
+  # 10 (personaje_meta, caja_meta, espacio)
     
   def jugar(self):
     while True:
