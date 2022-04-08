9420bbda29057a8549ad0226d562b28784a86e51
Miguel Angel Rodriguez Quirino <97248509+Quirinoz@users.noreply.github.com>
2022-04-08 18:03:10 +0000

    leer archivo

--- prueba.py
+++ prueba.py
@@ -1,3 +1,17 @@
 # TODO LEER ARCHIVO DE TEXTO
 # TODO IMPRIMIR ARCHIVO DE TEXTO
 # TODO ALMACENAR ARCHIVO DE TEXTO EN UNA MATRIZ
+
+class Archivos:
+
+  def __init__ (self):
+    pass
+
+  def leer (self,archivo):
+    file = open(archivo, 'r')
+    for line in file:
+      print(line)
+    file.close()
+
+objeto = Archivos()
+objeto.leer("nivel.txt")
